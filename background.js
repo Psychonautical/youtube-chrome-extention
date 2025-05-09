83eeb469838a872502b5345b9b36986ed1643c1a
István Bán <banistvan@example.com>
2025-05-09 13:19:07 +0200

    changed background_2.js to background.js

--- background.js
+++ background.js
@@ -4,17 +4,22 @@ let interval = null;
 
 function startTimer() {
     startTime = Date.now();
-    interval = setInterval(() => {
-        elapsedTime = Date.now() - startTime;
-        chrome.storage.local.set({ youtubeTime: elapsedTime });
-    }, 1000);
+    
+    // Retrieve stored time so we continue from where we left off
+    chrome.storage.local.get("youtubeTime", (data) => {
+        elapsedTime = data.youtubeTime ? data.youtubeTime : 0;
+
+        interval = setInterval(() => {
+            const newElapsedTime = elapsedTime + (Date.now() - startTime);
+            chrome.storage.local.set({ youtubeTime: newElapsedTime });
+        }, 1000);
+    });
 }
 
 function stopTimer() {
     if (interval) {
         clearInterval(interval);
         interval = null;
-        chrome.storage.local.set({ youtubeTime: elapsedTime });
     }
 }
 
